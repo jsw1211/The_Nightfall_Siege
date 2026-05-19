8eb6db578fdf178888578a29ee603aa30da6f99d
jsw1211 <jswtiger@naver.com>
2026-05-19 14:48:13 +0900

    스킬트리 UI 클래스 생성

---
+++ Source/The_Nightfall_Siege/Public/SkillTreeWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SkillTreeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THE_NIGHTFALL_SIEGE_API USkillTreeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
