201d4e171611acb392e446d361297842d54650ef
jsw1211 <jswtiger@naver.com>
2026-06-06 18:24:32 +0900

    swird 충돌 박스 설정 및 충돌시 로그

--- Source/The_Nightfall_Siege/Public/WeaponBase.h
+++ Source/The_Nightfall_Siege/Public/WeaponBase.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
 #include "WeaponBase.generated.h"
+
+class AMonster;
 
 UCLASS()
 class THE_NIGHTFALL_SIEGE_API AWeaponBase : public AActor
@@ -17,6 +20,12 @@ public:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UBoxComponent* WeaponCollision;
+
+	UFUNCTION()
+	void OnWeaponOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 protected:
 	// Called when the game starts or when spawned
