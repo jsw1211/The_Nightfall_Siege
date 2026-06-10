d5976aab90448d6fa3d5dc5660d540388fc91431
jsw1211 <jswtiger@naver.com>
2026-06-10 10:47:10 +0900

    게임모드 관련 코드 작성

---
+++ Source/The_Nightfall_Siege/Public/TheNightfallSiegeInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CharacterType.h"
+#include "TheNightfallSiegeInstance.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS()
+class THE_NIGHTFALL_SIEGE_API UTheNightfallSiegeInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+    UPROPERTY(BlueprintReadWrite, EditAnywhere)
+    ECharacterType SelectedCharacter = ECharacterType::Paladin;
+};
