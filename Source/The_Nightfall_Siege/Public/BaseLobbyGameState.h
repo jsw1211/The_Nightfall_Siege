692f708c0f49d3af294034fcb154ff68e7a80156
jsw1211 <jswtiger@naver.com>
2026-06-15 21:50:24 +0900

    로비 관련 코드 생성

---
+++ Source/The_Nightfall_Siege/Public/BaseLobbyGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "BaseLobbyGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THE_NIGHTFALL_SIEGE_API ABaseLobbyGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
